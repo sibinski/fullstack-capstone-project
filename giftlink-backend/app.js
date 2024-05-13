062c1e70f7974d8d0ac4707270ff82e7c4dd90e0
Jevgeni Shibinski <158762167+sibinski@users.noreply.github.com>
2024-05-13 08:52:04 +0300

    Initialize ul-m2-template

---
+++ giftlink-backend/app.js
@@ -0,0 +1,52 @@
+/*jshint esversion: 8 */
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const pinoLogger = require('./logger');
+
+const connectToDatabase = require('./models/db');
+const {loadData} = require("./util/import-mongo/index");
+
+
+const app = express();
+app.use("*",cors());
+const port = 3060;
+
+// Connect to MongoDB; we just do this one time
+connectToDatabase().then(() => {
+    pinoLogger.info('Connected to DB');
+})
+    .catch((e) => console.error('Failed to connect to DB', e));
+
+
+app.use(express.json());
+
+// Route files
+// Task 1: import the giftRoutes and store in a costant called giftroutes
+//{{insert code here}}
+const authRoutes = require('./routes/authRoutes');
+const searchRoutes = require('./routes/searchRoutes');
+const pinoHttp = require('pino-http');
+const logger = require('./logger');
+
+app.use(pinoHttp({ logger }));
+
+// Use Routes
+// Task 2: add the giftRoutes to the server by using the app.use() method.
+//{{insert code here}}
+app.use('/api/auth', authRoutes);
+app.use('/api/search', searchRoutes);
+
+// Global Error Handler
+app.use((err, req, res, next) => {
+    console.error(err);
+    res.status(500).send('Internal Server Error');
+});
+
+app.get("/",(req,res)=>{
+    res.send("Inside the server")
+})
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
