428e1b92f011beb757a631b17cc2c1106f89d65c
sibinski <jevgeni.shibinski@gmail.com>
2024-05-30 01:07:17 -0400

    Back-end Integration for Registration

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -28,6 +28,9 @@ const giftRoutes = require('./routes/giftRoutes');
 // Search API Task 1: import the searchRoutes and store in a constant called searchRoutes
 const searchRoutes = require('./routes/searchRoutes');
 
+//authRoute API:  import the authRoutes and store in a constant called authRoutes
+const authRoutes = require('./routes/authRoutes');
+
 const pinoHttp = require('pino-http');
 const logger = require('./logger');
 
@@ -39,6 +42,9 @@ app.use('/api/gifts', giftRoutes);
 
 // Search API Task 2: add the searchRoutes to the server by using the app.use() method.
 app.use('/api/search', searchRoutes);
+
+// auth API: add the authRoutes to the server by using the app.use() method.
+app.use('/api/auth', authRoutes);
 
 // Global Error Handler
 app.use((err, req, res, next) => {
