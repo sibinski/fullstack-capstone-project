c250d228abcadf87e1a0d1df3e2bc15e44f1152b
sibinski <jevgeni.shibinski@gmail.com>
2024-05-30 04:42:48 -0400

    Implement Back-end in Login Component

--- giftlink-frontend/src/components/LoginPage/LoginPage.js
+++ giftlink-frontend/src/components/LoginPage/LoginPage.js
@@ -1,4 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
+import { urlConfig } from '../../config';
+import { useAppContext } from '../../context/AuthContext';
+import { useNavigate } from 'react-router-dom';
+
 import './LoginPage.css';
 
 function LoginPage() {
@@ -6,11 +10,56 @@ function LoginPage() {
     //insert code here to create useState hook variables for email, password
     const [email, setEmail]  = useState('');
     const [password, setPassword] = useState('');
+    const [incorrect, setIncorrect] = useState('');
+    const navigate = useNavigate();
+    const bearerToken = sessionStorage.getItem('bearer-token');
+    const {setIsLoggedIn} = useAppContext();
+
+    useEffect(() => {
+        if (sessionStorage.getItem('auth-token')) {
+            navigate('/app')
+        }
+    }, [navigate])
 
     // insert code here to create handleLogin function and include console.log
-    const handleLogin = async () => {
+    const handleLogin = async (e) => {
         console.log("Inside handleLogin");
-    } 
+        e.preventDefault();
+        try {
+            const response = await fetch(`${urlConfig.backendUrl}/api/auth/login`, {
+                method: POST,
+                headers: {
+                    'content-type': 'application/json',
+                    'Authorization': bearerToken ? `Bearer $ {bearerToken}` : '',
+                },
+                body: JSON.stringify({
+                    email: email,
+                    password: password
+                })
+            });
+            
+                
+                const json = await res.json();
+                console.log('Json',json);
+                if (json.authtoken) 
+                try 
+                {
+                sessionStorage.setItem('auth-token', json.authtoken);
+                sessionStorage.setItem('name', json.userName);
+                sessionStorage.setItem('email', json.userEmail);
+                setIsLoggedIn(true);
+                navigate('/app');
+                }
+                else {
+                    document.getElementById("email").value="";
+                    document.getElementById("password").value="";
+                    setIncorrect("Wrong password. Try again.");
+                }
+                setTimeout(() => {
+                    setIncorrect("");
+                  }, 2000);
+                }
+            }
 
         return (
     <div className="container mt-5">
@@ -30,6 +79,18 @@ function LoginPage() {
             value={email}
             onChange={(e) => setEmail(e.target.value)}
     />
+    </div>
+    <div className="mb-4">
+        <label htmlFor="password" className="form-label">Password</label>
+        <input
+            id="password"
+            type="password"
+            className="form-control"
+            placeholder="Enter your password"
+            value={password}
+            onChange={(e) => {setPassword(e.target.value);setIncorrect("")}}
+    />
+    <span style={{color:'red',height:'.5cm',display:'block',fontStyle:'italic',fontSize:'12px'}}>{incorrect}</span>
     </div>
 
     <button className="btn btn-primary w-100 mb-3" onClick={handleLogin}>Login</button>
